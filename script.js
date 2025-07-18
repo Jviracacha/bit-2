96d8dd35590015c1eaaa517f3a8d4e2942e125fa
Jonathan Viracacha <jonathanviracacha@gmail.com>
2025-07-18 17:02:35 -0500

    update 3

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ $h2.style.color = "red"; // This is a hex color code for a shade of blue
 /* CRUD operations on the DOM */
 // Create, Read, Update, Delete
 $h2.textContent = "My first HTML document - Updated"; // Update the text content of the h2 element
+
+const $root = window.document.getElementById ('root')
+const $p = window.document.createElement ("p")
+$root.appendChild ($p)
+$p.textContent = 'parrafo de prueba'
+
+$p.remove ();
+
+
+$root.innerHTML = `<button> registrate </button>`;
+
+const $button = document.querySelector ('button');
+
+$button.addEventListener ('click', ()=> {
+console.log ('el usuario hizo click');
+
+});
+
+
+
+ 
+
+
 console.log($h2);
